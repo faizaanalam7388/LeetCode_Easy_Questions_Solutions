100a2d443c8c551ad6420d66451633e15372021e
Faizaan Alam <faizaanalam1980@gmail.com>
2026-08-22 23:31:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (80.46%) - LeetSync

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        
+        int step = 0;
+        
+        for(;num!=0;){
+            if(num%2==0){
+                num=num/2;
+            }
+            else{
+                num=num-1;
+            }
+
+            step++;
+        }
+
+        return step;
+        
+    }
+};
