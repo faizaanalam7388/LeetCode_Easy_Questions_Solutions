a85c0e1ed61320923471d1219db67f1190ce043c
Faizaan Alam <faizaanalam1980@gmail.com>
2026-09-03 23:42:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (61.12%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        
+        int i = 0, j = 0;
+        int n = version1.size();
+        int m = version2.size();
+
+        while (i < n || j < m) {
+            
+            int num1 = 0;
+            int num2 = 0;
+
+            // Read revision from version1
+            while (i < n && version1[i] != '.') {
+                num1 = num1 * 10 + (version1[i] - '0');
+                i++;
+            }
+
+            // Read revision from version2
+            while (j < m && version2[j] != '.') {
+                num2 = num2 * 10 + (version2[j] - '0');
+                j++;
+            }
+
+            if (num1 < num2)
+                return -1;
+
+            if (num1 > num2)
+                return 1;
+
+            // Skip dot
+            i++;
+            j++;
+        }
+
+        return 0;
+    }
+};
