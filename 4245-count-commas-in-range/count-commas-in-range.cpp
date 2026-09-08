5f3209eaff3fc2eeb402b9d2dcb8e1836a8e8559
Faizaan Alam <faizaanalam1980@gmail.com>
2026-09-08 23:39:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (15.51%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        if(n>999){
-            return n-999;
-        }
-        else{
-            return 0;
-        }
+        return max(n - 999, 0);
     }
 };
