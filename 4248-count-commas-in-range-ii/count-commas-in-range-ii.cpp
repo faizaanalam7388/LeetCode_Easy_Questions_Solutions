7fc57b3113452f0f839e597c705044f3b0bffb09
Faizaan Alam <faizaanalam1980@gmail.com>
2026-09-09 12:26:50 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (84.80%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -4,19 +4,19 @@ public:
         long long ans = 0;
 
         if (n >= 1000)
-            ans += n - 999;
+            ans = ans+n-999;
 
         if (n >= 1000000)
-            ans += n - 999999;
+            ans = ans+n-999999;
 
         if (n >= 1000000000)
-            ans += n - 999999999;
+            ans = ans+n-999999999;
 
         if (n >= 1000000000000LL)
-            ans += n - 999999999999LL;
+            ans = ans+n-999999999999LL;
 
         if (n >= 1000000000000000LL)
-            ans += n - 999999999999999LL;
+            ans = ans+n-999999999999999LL;
 
         return ans;
     }
